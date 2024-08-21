7730c10d1152c799f84febaa0267355a16f32e4f
shahrukhnizami <nizamishahrukh12@gmail.com>
2024-08-21 06:21:37 +0500

    green

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,4 @@
 function Button({text}){
-    return(<button className="flex-shrink-0 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-10 sm:mt-0" > {text}</button>)
+    return(<button className="flex-shrink-0 text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg mt-10 sm:mt-0" > {text}</button>)
 }
 export default Button;
